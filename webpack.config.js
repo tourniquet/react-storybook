111b83aef355c8244ac86aeabab68a58654e9efd
tourniquet <admyn3d@gmail.com>
2019-03-07 12:58:12 +0000

    Updated wepback config. Resolved correct path for components

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-stage-0', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react']
         }
       },
       {
